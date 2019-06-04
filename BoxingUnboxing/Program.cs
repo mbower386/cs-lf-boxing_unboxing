0255e9e8a030e78d104fb0e2ed27da1fe0a40cf7
mbower386 <mbower386@gmail.com>
2019-06-04 01:21:33 -0700

    finished

---
+++ BoxingUnboxing/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            List<object> listObject = new List<object> ();
+
+            listObject.Add (7);
+            listObject.Add (28);
+            listObject.Add (-1);
+            listObject.Add (true);
+            listObject.Add ("chair");
+
+            foreach (object l in listObject)
+            {
+                Console.WriteLine (l);
+            }
+
+            int sum = 0;
+            foreach (object l in listObject)
+            {
+                if (l is int)
+                {
+                    sum += (int)l;
+                }
+            }
+            Console.WriteLine (sum);
+        }
+    }
+}
